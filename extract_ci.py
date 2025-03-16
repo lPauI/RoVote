90300c3089c2974ca743083b857b4e8544266715
lPauI <lascupg13@gmail.com>
2025-03-16 06:06:17 +0200

    Made extract_ci files temporary

--- extract_ci.py
+++ extract_ci.py
@@ -1,6 +1,8 @@
 import cv2
 import pytesseract
 import re
+import tempfile
+import os
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -10,12 +12,20 @@ def find_cnp_from_ci(ci_path):
     gray, img_bin = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     gray = cv2.bitwise_not(img_bin)
 
-    new_image_save = 'processed_ci.jpg'
-    cv2.imwrite(new_image_save, gray)
-
-    extracted_text = pytesseract.image_to_string(new_image_save)
+    temp = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg")
+    temp_filename = temp.name
+    temp.close()
+    
+    cv2.imwrite(temp_filename, gray)
+    
+    extracted_text = pytesseract.image_to_string(temp_filename)
+    
+    os.unlink(temp_filename)
 
     possible_matches = re.search(r'\d{13}', extracted_text)
+    if not possible_matches:
+        return 'CNP was not found'
+        
     matches = re.search(r'^[1-6]\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])(?:(?!999)\d{3}|999)\d{3}$', possible_matches.group(0))
 
     if not matches:
